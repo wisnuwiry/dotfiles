7fae0639d3609f4ed58782356f66449c52242f55
Wisnu Ginanjar Saputra <wisnu@zero-one-group.com>
2022-12-30 19:46:17 +0700

    chore: Update option show git branch at nvimtree

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,20 +1,28 @@
+local overrides = require "custom.plugins.configs.overrides"
+
 -- Custom plugins
 return {
+	-- overrde plugin configs
+	["kyazdani42/nvim-tree.lua"] = {
+    override_options = overrides.nvimtree,
+  },
 
   -- Flutter
   ["akinsho/flutter-tools.nvim"] = {
     config = function()
-      require "custom.plugins.configs.flutter"
       local present, telescope = pcall(require, "telescope")
 
       if present then
+        require "custom.plugins.configs.flutter"
         telescope.load_extension("flutter")
       end
     end,
   },
 
   -- Debugger
-  ["mfussenegger/nvim-dap"] = {},
+  ["mfussenegger/nvim-dap"] = {
+    after = "flutter-tools"
+  },
 
   -- Wakatime
   ["wakatime/vim-wakatime"] = {},
